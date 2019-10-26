9243d02244bab531db7b6c0a9926ae4a909cae21
beyarkay <boydrkane@gmail.com>
2019-10-26 13:09:15 +0200

    Add number formatting and include the link in the {house}

--- main.py
+++ main.py
@@ -18,27 +18,48 @@ def main():
         price = content.find_all("div", class_="p24_price")
         description = content.find_all("div", class_="p24_description")
         if description:
+
+            if content.parent.has_attr("href"):
+                house["link"] = "https://www.property24.com" + content.parent["href"]
             icons = content.find("div", class_="p24_icons")
             attribs = icons.find_all("span")
             for j, attrib in enumerate(attribs):
                 if attrib.has_attr("title"):
                     house[attrib["title"].lower()] = attrib.find("span").text
 
-            # house["bedrooms"] = get_icon_attr(icons, "Bedrooms")
-            # house["bathrooms"] = get_icon_attr(icons, "Bathrooms")
-            # house["garages"] = get_icon_attr(icons, "Garages")
-            # house["sq_meters"] = get_icon_attr(icons, "Floor Size")
-
-            house["price"] = int(normalise_from_findall(price)[2:])
+            house["price"] = normalise_from_findall(price)[2:]
             house["description"] = normalise_from_findall(description)
             location = description[0].find_all("span", class_="p24_location")
             house["location"] = normalise_from_findall(location)
+
+            if "bathrooms" in house:
+                house["bathrooms"] = format_numbers(house["bathrooms"])
+            if "bedrooms" in house:
+                house["bedrooms"] = format_numbers(house["bedrooms"])
+            if "erf size" in house:
+                house["erf size"] = format_numbers(house["erf size"])
+            if "floor size" in house:
+                house["floor size"] = format_numbers(house["floor size"])
+            if "garages" in house:
+                house["garages"] = format_numbers(house["garages"])
+            if "price" in house:
+                house["price"] = format_numbers(house["price"])
+
         else:
-            # print(f"No description found, content:\n{content.prettify()}")
-            pass
+            print(f"No description found, content:\n{content.prettify()}")
+            # pass
 
         pprint(house)
         print()
+
+
+def format_numbers(s):
+    # print(s)
+    s = str(s).replace("m²", "").replace(" ", "").strip()
+    if s.replace(".", "", 1).isdigit():
+        return float(s)
+    else:
+        raise TypeError(f"{s} is not an integer")
 
 
 def get_icon_attr(icons, attr):
