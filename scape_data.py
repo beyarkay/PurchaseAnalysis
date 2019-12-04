f8cbe27b8ec600df97094b0645b841661b8ad37f
beyarkay <boydrkane@gmail.com>
2019-12-04 16:51:33 +0200

    Start refactoring for raspberry pi automation

---
+++ scape_data.py
@@ -0,0 +1,28 @@
+import sys
+DOWNLOAD_HTML = False
+
+def scrape_cars():
+
+
+    pass
+
+
+def scrape_houses():
+    pass
+
+
+def dev_main():
+    pass
+
+
+def main():
+    projects = sys.argv[1:]
+    if "cars" in projects:
+        scrape_cars()
+    if "houses" in projects:
+        scrape_houses()
+
+
+if __name__ == '__main__':
+    print(sys.argv)
+    main()
