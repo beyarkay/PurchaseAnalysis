17b34736d78c80af0530eab9cf6e6ae146c31571
beyarkay <boydrkane@gmail.com>
2019-12-07 13:30:30 +0200

    Automatic update to cars.csv

--- update_cars_csv.sh
+++ update_cars_csv.sh
@@ -2,7 +2,7 @@
 
 cd ~/git_repos/PurchaseAnalysis
 git pull
-python ~/git_repos/PurchaseAnalysis/project_files/cars/scrape_cars.py
+python -u ~/git_repos/PurchaseAnalysis/project_files/cars/scrape_cars.py
 git add .
 git commit -m "Automatic update to cars.csv"
 git push -u origin master
