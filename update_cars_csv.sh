dbbd3aefddbecded53535f64c9cae93b5b645f32
beyarkay <boydrkane@gmail.com>
2019-12-31 00:39:54 +0200

    Automatic update from raspberry pi

--- update_cars_csv.sh
+++ update_cars_csv.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 cd ~/git_repos/PurchaseAnalysis
+echo "-----------PULLING FROM REPO-----------"
 git pull
+echo "-----------EXECUTING SCRIPT------------"
 python3 -u ~/git_repos/PurchaseAnalysis/project_files/cars/scrape_cars.py
 git add .
-git commit -m "Automatic update to cars.csv"
+git commit -m "Automatic update from raspberry pi"
+echo "-----------PUSHING TO REPO-------------"
 git push -u origin master
