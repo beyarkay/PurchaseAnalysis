559dfb3a727b9c0f7d7732789388b4923cc8a06b
Boyd Kane <33420535+beyarkay@users.noreply.github.com>
2020-01-06 10:31:47 +0200

    Update update_cars_db.sh

--- update_cars_db.sh
+++ update_cars_db.sh
@@ -2,12 +2,12 @@
 
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 echo "=====================$dt====================="
-ip route
+# ip route
 cd ~/git_repos/PurchaseAnalysis
 source venv/bin/activate
 echo "-----------PULLING FROM REPO-----------"
-git add .
-git commit -m "Beep Boop: Pre-commit before rebase"
+# git add .
+# git commit -m "Beep Boop: Pre-commit before rebase"
 git pull --rebase
 echo "-----------UPDATING REQUIREMENTS-----------"
 pip install -r requirements.txt
