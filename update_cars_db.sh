fca3542599870b6784001a83f206e39b50896b5f
beyarkay <boydrkane@gmail.com>
2020-01-24 00:00:03 +0200

    Beep Boop: Pre-commit before rebase

--- update_cars_db.sh
+++ update_cars_db.sh
@@ -14,7 +14,7 @@ echo "-----------EXECUTING SCRIPT------------"
 python3 -u ~/git_repos/PurchaseAnalysis/project_files/cars/scrape_cars.py
 git add . > /dev/null
 git commit -m "Beep Boop: Automatic update from raspberry pi" > /dev/null
-echo "-----------PUSHING TO REPO-------------"
+echo "-----------PUSHING TO REPO-------------" > /dev/null
 git pull --rebase > /dev/null
 git push -u origin master > /dev/null
 deactivate
